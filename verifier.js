df672831ec942f9b2cd1e5983923565e8561889f
Samuel Rinnetmäki <samuel@iki.fi>
2024-10-28 15:12:01 +0200

    givenName => given_name

--- verifier.js
+++ verifier.js
@@ -48,7 +48,7 @@ async function createRequest(id) {
           "fields": [
             {
               "path": [
-                "$.credentialSubject.Person.givenName",
+                "$.credentialSubject.Person.given_name",
               ]
             },
 /*
